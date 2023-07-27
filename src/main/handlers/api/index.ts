a215a01354335a53b8fd80e9071f05453c498e8d
rafagodoy <gdiniz.ti@gmail.com>
2023-07-27 08:49:44 -0300

    feature: add new factory and handler for
    - connect with POST /financial-products endpoint

--- src/main/handlers/api/index.ts
+++ src/main/handlers/api/index.ts
@@ -2,5 +2,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { AWSLambdaAdapter } from '../../aws-lambda.adapter';
+import { 
+  makeCreateFinancialProductController,
+} from '../../factories/api/create-financial-product.factory';
 
-console.log(AWSLambdaAdapter);
+module.exports.createFinancialProduct = async (event) => {
+
+  const controller = makeCreateFinancialProductController();
+  const awsLambda = new AWSLambdaAdapter(controller);
+
+  return awsLambda.start(event);
+  
+};
