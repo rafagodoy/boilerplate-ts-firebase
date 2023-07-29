ceb685c5acf030ec432c25085990b0366cc6fdd6
rafagodoy <gdiniz.ti@gmail.com>
2023-07-29 12:01:00 -0300

    chore: change configs for
    - cleaning unnecessary endpoints
    
    - change Aurora for Firebase infra implementation
    
    - return a mock database response

--- src/infra/databases/aurora.ts
+++
@@ -1,8 +0,0 @@
-import { Database } from '@/adapters/repository/protocols';
-
-export class AuroraDb<T, U> implements Database<T, U> {
-  async insert(input: T): Promise<U> {
-    console.log(input);
-    return null;
-  }
-}
---
+++ src/infra/databases/firebase.ts
@@ -0,0 +1,18 @@
+import { Database } from '@/adapters/repository/protocols';
+
+export class Firebase<T, U> implements Database<T, U> {
+  async insert(input: T): Promise<U> {
+    console.log(input);
+    return {
+      id: 222,
+      productName: 'test',
+      institutionName: 'test',
+      retainability: 'test',
+      expirationDate: 'test',
+      productRating: 'test',
+      reportUrl: 'test',
+      createdAt: 'test',
+      updatedAt: 'test',
+    } as U;
+  }
+}
