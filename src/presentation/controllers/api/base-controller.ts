35ae3541259e8749e863a6d846b0379eed7fc8f8
rafagodoy <gdiniz.ti@gmail.com>
2023-09-12 08:06:34 -0300

    refactor: getting data request from BaseController

--- src/presentation/controllers/api/base-controller.ts
+++ src/presentation/controllers/api/base-controller.ts
@@ -12,11 +12,27 @@ export class BaseController implements HttpController  {
     private readonly requestValidator: HttpRequestValidator,
   ) {}
 
+  getDataFromRequest(httpRequest: HttpRequest): object {
+
+    let body = null;
+    let queryStringParams = null;
+
+    if (Object.keys(httpRequest.queryStringParams).length !== 0) {
+      queryStringParams = httpRequest.queryStringParams;
+    }
+
+    if (Object.keys(httpRequest.body).length !== 0) {
+      body = httpRequest.body;
+    }
+
+    return body || queryStringParams;
+  }
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
           
-    const { body } = httpRequest;
+    const data = this.getDataFromRequest(httpRequest);
 
-    const schemaErrors = await this.requestValidator.validate(body);
+    const schemaErrors = await this.requestValidator.validate(data);
 
     if (schemaErrors) {
       return {
